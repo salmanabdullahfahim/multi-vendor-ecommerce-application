8ad091e781db4761abe033ab70f136a82b010d1d
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-12-01 19:37:22 +0600

    best selling chart added

--- src/components/backend-ui/BestSellingChart.jsx
+++ src/components/backend-ui/BestSellingChart.jsx
@@ -1,9 +1,38 @@
+"use client";
 import React from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export const data = {
+  labels: ["Onion", "Carrot", "Cucumber", "Potato"],
+  datasets: [
+    {
+      label: "# of Sales",
+      data: [120, 140, 119, 34],
+      backgroundColor: [
+        "rgb(196 181 253)",
+        "rgb(99, 102, 241)",
+        "rgb(20 184 166)",
+        "rgba(75, 192, 192, 0.2)",
+      ],
+      borderColor: [
+        "rgb(196 181 253)",
+        "rgb(99, 102, 241)",
+        "rgb(20 184 166)",
+        "rgba(75, 192, 192, 1)",
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
 
 const BestSellingChart = () => {
   return (
     <div className="bg-slate-700 p-8 rounded-lg">
-      <h2 className="text-xl font-bold">Best selling products</h2>
+      <h2 className="text-xl font-bold mb-4">Best selling products</h2>
+      <Pie data={data} />
     </div>
   );
 };
