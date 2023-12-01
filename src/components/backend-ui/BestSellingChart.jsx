bfbfd4275c50bc6f71534cafdaea80ba6c93efed
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-12-01 18:30:10 +0600

    added chartjs 2 package for chart

--- src/components/backend-ui/BestSellingChart.jsx
+++ src/components/backend-ui/BestSellingChart.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const BestSellingChart = () => {
-  return <div>BestSellingChart</div>;
+  return (
+    <div className="bg-slate-700 p-8 rounded-lg">
+      <h2 className="text-xl font-bold">Best selling products</h2>
+    </div>
+  );
 };
 
 export default BestSellingChart;
